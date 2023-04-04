62220212ef480e1c2ee6d2ada0e104eb28f1ef66
mecaneer23 <mecaneer23@gmail.com>
2023-04-03 19:15:19 -0500

    allow number input for color menu

--- todo.py
+++ todo.py
@@ -343,6 +343,8 @@ def color_menu(parent_win):
             return
         elif key == 10:  # enter
             return get_color(lines[selected].strip())
+        elif key in range(49, 56):
+            selected = key - 49
         else:
             continue
         selected = ensure_within_bounds(selected, 0, len(lines))
