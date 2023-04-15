f62b748fcf4b8f6268602e060adf813aafc2914f
mecaneer23 <mecaneer23@gmail.com>
2023-04-15 13:35:40 -0500

    make sure extra characters aren't echoed

--- tools/keytester.py
+++ tools/keytester.py
@@ -4,13 +4,14 @@ import curses
 
 
 def main(stdscr):
+    curses.curs_set(0)
     stdscr.addstr("Ctrl+C to exit\n")
     while True:
         try:
             ch = stdscr.getch()
         except KeyboardInterrupt:
             return
-        stdscr.addstr(*[i // 2 for i in stdscr.getmaxyx()], str(ch))
+        stdscr.addstr(*[i // 2 for i in stdscr.getmaxyx()], str(ch) + "      ")
 
 
 if __name__ == "__main__":
