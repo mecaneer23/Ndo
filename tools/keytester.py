674c510f1161f69ec600c87df744049d5508e4b6
mecaneer23 <mecaneer23@gmail.com>
2023-04-15 12:48:00 -0500

    format keytester

--- tools/keytester.py
+++ tools/keytester.py
@@ -2,14 +2,16 @@
 
 import curses
 
+
 def main(stdscr):
-	stdscr.addstr("Ctrl+C to exit\n")
-	while True:
-		try:
-			ch = stdscr.getch()
-		except KeyboardInterrupt:
-			return
-		stdscr.addstr(*[i//2 for i in stdscr.getmaxyx()], str(ch))
+    stdscr.addstr("Ctrl+C to exit\n")
+    while True:
+        try:
+            ch = stdscr.getch()
+        except KeyboardInterrupt:
+            return
+        stdscr.addstr(*[i // 2 for i in stdscr.getmaxyx()], str(ch))
+
 
 if __name__ == "__main__":
-	curses.wrapper(main)
+    curses.wrapper(main)
