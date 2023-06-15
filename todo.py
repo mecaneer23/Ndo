5dea8197507a3e33db20ff93ea2b897adf576944
mecaneer23 <mecaneer23@gmail.com>
2023-06-14 23:18:01 -0500

    fix edit when text is as long as textbox

--- todo.py
+++ todo.py
@@ -303,12 +303,12 @@ def wgetnstr(win, n=1024, chars="", cursor="█"):
     win.box()
     win.nodelay(False)
     while True:
-        for i, v in enumerate("".join(chars).ljust(win.getmaxyx()[1] - 3)):
-            win.addstr(1, i + 1, v, curses.A_REVERSE if i == position else 0)
         if position == len(chars):
             if len(chars) + 1 >= win.getmaxyx()[1] - 1:
                 return "".join(chars)
             win.addstr(1, len(chars) + 1, cursor)
+        for i, v in enumerate("".join(chars).ljust(win.getmaxyx()[1] - 2)):
+            win.addstr(1, i + 1, v, curses.A_REVERSE if i == position else 0)
         win.refresh()
         try:
             ch = win.getch()
