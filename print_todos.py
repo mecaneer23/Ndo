4f06a75a35d7500957b40f73497b8f8c3b63659d
mecaneer23 <mecaneer23@gmail.com>
2023-09-08 11:14:54 -0500

    move print_todos out of todo.py

---
+++ print_todos.py
@@ -0,0 +1,157 @@
+# pylint: disable=global-statement, missing-class-docstring
+# pylint: disable=missing-function-docstring, missing-module-docstring
+
+from typing import Any, TypeVar
+from os import get_terminal_size
+import curses
+
+from class_todo import Todo
+from class_cursor import Cursor
+
+T = TypeVar("T")
+
+BULLETS = False
+ENUMERATE = False
+INDENT = 0
+NO_GUI = False
+RELATIVE_ENUMERATE = False
+SIMPLE_BOXES = False
+STRIKETHROUGH = False
+
+
+def init(
+    bullets,
+    this_enumerate,
+    indent,
+    no_gui,
+    relative_enumerate,
+    simple_boxes,
+    should_strikethrough,
+):
+    global BULLETS
+    global ENUMERATE
+    global INDENT
+    global NO_GUI
+    global RELATIVE_ENUMERATE
+    global SIMPLE_BOXES
+    global STRIKETHROUGH
+
+    BULLETS = bullets
+    ENUMERATE = this_enumerate
+    INDENT = indent
+    NO_GUI = no_gui
+    RELATIVE_ENUMERATE = relative_enumerate
+    SIMPLE_BOXES = simple_boxes
+    STRIKETHROUGH = should_strikethrough
+
+
+def get_bullet(indentation_level: int) -> str:
+    symbols = [
+        "•",
+        "◦",
+        "▪",
+        # "▫",
+    ]
+    return symbols[indentation_level // INDENT % len(symbols)]
+
+
+def strikethrough(text: str) -> str:
+    return "\u0336".join(text) if STRIKETHROUGH else text
+
+
+def make_printable_sublist(
+    height: int, lst: list[T], cursor: int, distance: int = -1
+) -> tuple[list[T], int]:
+    if len(lst) < height:
+        return lst, cursor
+    distance = height * 3 // 7 if distance < 0 else distance
+    start = max(0, cursor - distance)
+    end = min(len(lst), start + height)
+    # If len(sublist) < height, stop moving list and resume moving cursor
+    if end - start < height:
+        start = len(lst) - height
+        end = len(lst)
+    return lst[start:end], cursor - start
+
+
+def print_todos(win: Any, todos: list[Todo], selected: Cursor) -> None:
+    if win is None:
+        width, height = get_terminal_size()
+    else:
+        height, width = win.getmaxyx()
+    new_todos, temp_selected = make_printable_sublist(height - 1, todos, int(selected))
+    highlight = range(temp_selected, len(selected) + temp_selected)
+    for relative, (i, todo) in zip(
+        [*range(temp_selected - 1, -1, -1), int(selected), *range(0, len(new_todos))],
+        enumerate(new_todos),
+    ):
+        if todo.color is None:
+            raise ValueError(f"Invalid color for `{todo}`")
+        display_string = (
+            "".join(
+                [
+                    todo.indent_level * " ",
+                    todo.get_box() if not todo.is_empty() and not SIMPLE_BOXES else "",
+                    todo.get_simple_box()
+                    if not todo.is_empty() and SIMPLE_BOXES
+                    else "",
+                    (
+                        f"{get_bullet(todo.indent_level)} "
+                        if not todo.has_box() and BULLETS
+                        else ""
+                    ),
+                    (
+                        f"{todos.index(todo) + 1}. "
+                        if ENUMERATE and not RELATIVE_ENUMERATE
+                        else ""
+                    ),
+                    f"{relative + 1}. " if RELATIVE_ENUMERATE else "",
+                    (
+                        strikethrough(todo.display_text)
+                        if todo.is_toggled()
+                        else todo.display_text
+                    ),
+                ]
+            )
+            if i not in highlight or not todo.is_empty()
+            else "⎯" * 8
+        )[: width - 1].ljust(width - 1, " ")
+        counter = 0
+        if win is None:
+            print(
+                {
+                    1: "\u001b[31m",
+                    2: "\u001b[32m",
+                    3: "\u001b[33m",
+                    4: "\u001b[34m",
+                    5: "\u001b[35m",
+                    6: "\u001b[36m",
+                    7: "\u001b[37m",
+                }[todo.color]
+                + display_string
+                + "\u001b[0m"
+            )
+            continue
+        while counter < len(display_string) - 1:
+            try:
+                win.addch(
+                    i + 1,
+                    counter,
+                    display_string[counter],
+                    curses.color_pair(todo.color or 7)
+                    | (curses.A_REVERSE if i in highlight else 0),
+                )
+            except OverflowError:
+                # This function call will throw an OverflowError if
+                # the terminal doesn't support the box character as it
+                # is technically a wide character. By `continue`-ing,
+                # we don't print the box character and indirectly
+                # prompt the user to use the -x option and use simple
+                # boxes when printing.
+                counter += 1
+                continue
+            counter += 1
+    if win is None:
+        return
+    for i in range(height - len(new_todos) - 1):
+        win.addstr(i + len(new_todos) + 1, 0, " " * (width - 1))
--- todo.py
+++ todo.py
@@ -4,11 +4,10 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring
 
 import curses
-from os import get_terminal_size
 from pathlib import Path
 from re import match as re_match
 from sys import exit as sys_exit
-from typing import Any, Callable, TypeVar
+from typing import Any, Callable
 
 from pyfiglet import figlet_format as big
 from pyperclip import copy, paste
@@ -23,8 +22,9 @@ from get_args import init as init_get_args
 from get_todo import hline, set_header, wgetnstr
 from get_todo import init as init_wgetnstr
 from md_to_py import md_table_to_lines
+from print_todos import print_todos, make_printable_sublist
+from print_todos import init as init_print_todos
 
-T = TypeVar("T")
 BULLETS = False
 CONTROLS_BEGIN_INDEX = 55
 CONTROLS_END_INDEX = 79
@@ -138,10 +138,6 @@ def remove_todo(todos: list[Todo], index: int) -> list[Todo]:
         return todos
     todos.pop(index)
     return todos
-
-
-def strikethrough(text: str) -> str:
-    return "\u0336".join(text) if STRIKETHROUGH else text
 
 
 def swap_todos(todos: list[Todo], idx1: int, idx2: int) -> list[Todo]:
@@ -343,123 +339,6 @@ def sort_menu(
         cursor = clamp(cursor, 0, len(lines))
         parent_win.refresh()
         win.refresh()
-
-
-def make_printable_sublist(
-    height: int, lst: list[T], cursor: int, distance: int = -1
-) -> tuple[list[T], int]:
-    if len(lst) < height:
-        return lst, cursor
-    distance = height * 3 // 7 if distance < 0 else distance
-    start = max(0, cursor - distance)
-    end = min(len(lst), start + height)
-    # If len(sublist) < height, stop moving list and resume moving cursor
-    if end - start < height:
-        start = len(lst) - height
-        end = len(lst)
-    return lst[start:end], cursor - start
-
-
-def print_todos(win: Any, todos: list[Todo], selected: Cursor) -> None:
-    if win is None:
-        width, height = get_terminal_size()
-    else:
-        height, width = win.getmaxyx()
-    new_todos, temp_selected = make_printable_sublist(height - 1, todos, int(selected))
-    highlight = range(temp_selected, len(selected) + temp_selected)
-    for relative, (i, todo) in zip(
-        [*range(temp_selected - 1, -1, -1), int(selected), *range(0, len(new_todos))],
-        enumerate(new_todos),
-    ):
-        if todo.color is None:
-            raise ValueError(f"Invalid color for `{todo}`")
-        display_string = (
-            "".join(
-                [
-                    todo.indent_level * " ",
-                    todo.get_box() if not todo.is_empty() and not SIMPLE_BOXES else "",
-                    todo.get_simple_box()
-                    if not todo.is_empty() and SIMPLE_BOXES
-                    else "",
-                    (
-                        f"{get_bullet(todo.indent_level)} "
-                        if not todo.has_box() and BULLETS
-                        else ""
-                    ),
-                    (
-                        f"{todos.index(todo) + 1}. "
-                        if ENUMERATE and not RELATIVE_ENUMERATE
-                        else ""
-                    ),
-                    f"{relative + 1}. " if RELATIVE_ENUMERATE else "",
-                    (
-                        strikethrough(todo.display_text)
-                        if todo.is_toggled()
-                        else todo.display_text
-                    ),
-                ]
-            )
-            if i not in highlight or not todo.is_empty()
-            else "⎯" * 8
-        )[: width - 1].ljust(width - 1, " ")
-        counter = 0
-        if win is None:
-            print(
-                {
-                    1: "\u001b[31m",
-                    2: "\u001b[32m",
-                    3: "\u001b[33m",
-                    4: "\u001b[34m",
-                    5: "\u001b[35m",
-                    6: "\u001b[36m",
-                    7: "\u001b[37m",
-                }[todo.color]
-                + display_string
-                + "\u001b[0m"
-            )
-            continue
-        while counter < len(display_string) - 1:
-            try:
-                win.addch(
-                    i + 1,
-                    counter,
-                    display_string[counter],
-                    curses.color_pair(todo.color or get_color("White"))
-                    | (curses.A_REVERSE if i in highlight else 0),
-                )
-            except OverflowError:
-                # This function call will throw an OverflowError if
-                # the terminal doesn't support the box character as it
-                # is technically a wide character. By `continue`-ing,
-                # we don't print the box character and indirectly
-                # prompt the user to use the -x option and use simple
-                # boxes when printing.
-                counter += 1
-                continue
-            counter += 1
-    if win is None:
-        return
-    for i in range(height - len(new_todos) - 1):
-        win.addstr(i + len(new_todos) + 1, 0, " " * (width - 1))
-
-
-def get_bullet(indentation_level: int) -> str:
-    """
-    Get the bullet symbol based on the indentation level.
-
-    Args:
-        indentation_level (int): The indentation level of the todo item.
-
-    Returns:
-        str: The bullet symbol to represent the todo item.
-    """
-    symbols = [
-        "•",
-        "◦",
-        "▪",
-        # "▫",
-    ]
-    return symbols[indentation_level // INDENT % len(symbols)]
 
 
 def todo_from_clipboard(
@@ -1005,6 +884,15 @@ if __name__ == "__main__":
     SIMPLE_BOXES = command_line_args.simple_boxes
     STRIKETHROUGH = command_line_args.strikethrough
     del command_line_args
+    init_print_todos(
+        BULLETS,
+        ENUMERATE,
+        INDENT,
+        NO_GUI,
+        RELATIVE_ENUMERATE,
+        SIMPLE_BOXES,
+        STRIKETHROUGH,
+    )
     if NO_GUI:
         print(f"{HEADER}:")
         print_todos(None, validate_file(read_file(FILENAME)), Cursor(0))
