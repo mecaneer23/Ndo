dfbeebad86832fe20ddd3d8a53aca0687dbe7046
mecaneer23 <mecaneer23@gmail.com>
2024-01-25 20:25:55 -0600

    refactor: move md_to_py docs into md_to_py.py

--- src/md_to_py.py
+++ src/md_to_py.py
@@ -35,27 +35,75 @@ def md_table_to_lines(
     remove: tuple[str, ...] = (),
 ) -> list[str]:
     """
-    Converts a Markdown table to a list of formatted strings.
+    Convert a Markdown table to a list of formatted strings.
 
-    Args:
-        first_line_idx (int): The index of the first line of the Markdown
-        table to be converted.
-        last_line_idx (int): The index of the last line of the Markdown
-        table to be converted.
-        filename (str, optional): The name of the markdown file containing the
-        table. Default is "README.md".
-        remove (list[str], optional): The list of strings to be removed from each line.
-        This is in the case of formatting that should exist in markdown but not
-        python. Default is an empty list.
+    Arguments
+    ---------
 
-    Returns:
-        list[str]: A list of formatted strings representing the converted
-        Markdown table.
+    - `first_line_idx` (int): The index of the first line of the markdown
+    table to be converted.
+    - `last_line_idx` (int): The index of the last line of the markdown
+    table to be converted.
+    - `filename` (str, optional): The name of the markdown file
+    containing the table. Default is “README.md”.
+    - `remove` (tuple[str], optional): The list of characters to be
+    removed from each line. Default is an empty list.
 
-    Raises:
-        ValueError: If the last line index is less than or equal to the
-        first line index.
-        FileNotFoundError: If the specified markdown file cannot be found.
+    Returns
+    -------
+
+    -  `list[str]`: A list of formatted strings representing the converted
+    Markdown table.
+
+    Raises
+    ------
+
+    -  `ValueError`: If the last line index is less than or equal to the
+    first line index.
+    -  `FileNotFoundError`: If the specified markdown file cannot be
+    found.
+    """
+
+    _ = """
+    ## Examples
+
+    | Item      | Quantity | Price |
+    | --------- | -------- | ----- |
+    | Apple     | 5        | $1.00 |
+    | Banana    | 3        | $1.50 |
+    | Orange    | 2        | $0.75 |
+    | Pineapple | 1        | $3.50 |
+
+    ```python
+    >>> print("\n".join(md_table_to_lines(23, 29)))
+    Item       Quantity  Price
+    --------------------------
+    Apple      5         $1.00
+    Banana     3         $1.50
+    Orange     2         $0.75
+    Pineapple  1         $3.50
+
+    ```
+
+    | Flag            | Description                     |
+    | --------------- | ------------------------------- |
+    | **-h**          | Display help message            |
+    | **-v**          | Enable verbose output           |
+    | **-f** FILENAME | Specify input file              |
+    | **-o** FILENAME | Specify output file             |
+    | **-n**          | Do not overwrite existing files |
+
+    ```python
+    >>> print("\n".join(md_table_to_lines(41, 48, remove=("**"))))
+    Flag         Description
+    --------------------------------------------
+    -h           Display help message
+    -v           Enable verbose output
+    -f FILENAME  Specify input file
+    -o FILENAME  Specify output file
+    -n           Do not overwrite existing files
+
+    ```
     """
 
     # Check for valid line indices
