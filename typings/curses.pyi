5cb7ba859932a754f1fffc6fb632a9b895f11aad
mecaneer23 <mecaneer23@gmail.com>
2024-02-23 21:23:47 -0600

    hack: move window definition outside of version constraint

--- typings/curses.pyi
+++ typings/curses.pyi
@@ -598,4 +598,4 @@ if sys.version_info >= (3, 8):
         minor: int
         patch: int
     ncurses_version: _ncurses_version
-    window = _CursesWindow  # undocumented
+window = _CursesWindow  # undocumented
