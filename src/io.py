46b06884559a3bffa283fa6035c0ef1a183fb617
mecaneer23 <mecaneer23@gmail.com>
2024-03-10 10:14:43 -0500

    refactor: add docstrings to public functions in `io.py`

--- src/io.py
+++ src/io.py
@@ -10,6 +10,12 @@ from src.class_todo import Todo, Todos
 
 
 def read_file(filename: Path) -> str:
+    """
+    Attempt to read a file and return its contents.
+
+    If the file does not exist, create it and return
+    an empty string.
+    """
     if not filename.exists():
         with filename.open("w"):
             return ""
@@ -18,11 +24,13 @@ def read_file(filename: Path) -> str:
 
 
 def file_string_to_todos(raw_data: str) -> Todos:
+    """Convert a TextIOWrapper to a list of `Todo`s"""
     if len(raw_data) == 0:
         return Todos([])
     return Todos([Todo(line) for line in raw_data.split("\n")])
 
 
 def update_file(filename: Path, lst: Todos) -> int:
+    """Write a list of `Todo`s to a provided file"""
     with filename.open("w", newline="\n") as file_obj:
         return file_obj.write("\n".join(map(repr, lst)))
