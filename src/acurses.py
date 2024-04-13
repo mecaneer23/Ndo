4b6221a7fc6fd2d4622584e08dc293c851d2b006
Mecaneer23 <74385377+Mecaneer23@users.noreply.github.com>
2024-04-13 12:07:28 -0500

    feat: add test to acurses

--- src/acurses.py
+++ src/acurses.py
@@ -12,8 +12,6 @@ _T = TypeVar("_T")
 
 
 class _CursesWindow:  # pylint: disable=too-many-instance-attributes
-    # def __init__(self) -> None:
-
     def getch(self) -> int:
         """
         Get a character. Note that the integer returned
@@ -59,3 +57,40 @@ def wrapper(func: Callable[..., _T], /, *args: list[Any], **kwds: dict[str, Any]
         #     curses.echo()
         #     curses.nocbreak()
         #     curses.endwin()
+
+
+def _main(stdscr: window):
+    # curses.use_default_colors()
+    # for i, color in enumerate(
+    #     [
+    #         curses.COLOR_RED,
+    #         curses.COLOR_GREEN,
+    #         curses.COLOR_YELLOW,
+    #         curses.COLOR_BLUE,
+    #         curses.COLOR_MAGENTA,
+    #         curses.COLOR_CYAN,
+    #         curses.COLOR_WHITE,
+    #     ],
+    #     start=1,
+    # ):
+    #     curses.init_pair(i, color, -1)
+    # scr.addstr(10, 1, "Hello, world!", curses.color_pair(7))
+    # scr.box()
+    # win = curses.newwin(3, 20, 10, 10)
+    # win.clear()
+    # win.box()
+    # win.addstr(1, 1, "Bold text", curses.color_pair(2))
+    # win.clear()
+    # scr.addstr(1, 1, "Bold text", curses.color_pair(5) | curses.A_STANDOUT)
+    while True:
+        x = stdscr.getch()
+        print(x)
+        if x == 27:
+            y = stdscr.getch()
+            print(str(y) + ":")
+        if x == 113:
+            break
+
+
+if __name__ == "__main__":
+    wrapper(_main)
